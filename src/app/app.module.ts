6c391150551b2f7c9ef197a7646fc1b275ef591c
Andi Meier <andi@daemeier-clan.ch>
2018-05-23 20:20:34 +0200

    app: add HttpClient...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { InMemoryDataService }  from './in-memory-data.service';
 import { LoginSelectorComponent } from './login-selector/login-selector.component';
 import { TestResultsComponent } from './test-results/test-results.component';
 
@@ -24,6 +27,9 @@ const routes: Routes = [
   imports: [
     BrowserModule,
     FormsModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }),
     NgbModule.forRoot(),
     RouterModule.forRoot(routes, { enableTracing: false }),
   ],
